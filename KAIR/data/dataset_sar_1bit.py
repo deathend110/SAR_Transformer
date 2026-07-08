0caee2437899fef1e5a84e87c09e8a93ae75dc9a
deathend110 <2735514990@qq.com>
2026-07-08 10:39:44 +0800

    新建数据集加载代码

---
+++ KAIR/data/dataset_sar_1bit.py
@@ -0,0 +1,25 @@
+import torch.utils.data as data
+import utils.utils_image as util
+
+# -----------------------------------------
+# one-bit SAR image restoration dataset
+
+class DatasetSAR1bit(data.Dataset):
+    """
+    Read one-bit SAR image pairs.
+    """
+
+    def __init__(self, opt):
+        super(DatasetSAR1bit, self).__init__()
+        pass
+
+    def __getitem__(self, index):
+        pass
+
+    def __len__(self):
+        pass
+
+
+
+
+
